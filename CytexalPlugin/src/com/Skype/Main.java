0b9c42806cf7fa809cef880dabb112d055dad0f5
Mika <mikaspecht@yahoo.de>
2016-04-30 22:59:33 +0200

    +Event

--- CytexalPlugin/src/com/Skype/Main.java
+++ CytexalPlugin/src/com/Skype/Main.java
@@ -2,14 +2,15 @@ package com.Skype;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() {
 		
 		new MoveEvent(this);
-		
+		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 	@Override
 	public void onDisable() {
